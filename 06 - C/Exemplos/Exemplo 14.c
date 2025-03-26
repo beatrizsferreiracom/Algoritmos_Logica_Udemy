ad584d11af0c7000a4907c68301d58d78f4d0db4
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2025-03-25 22:57:43 -0300

    Add files via upload

---
+++ 06 - C/Exemplos/Exemplo 14.c
@@ -0,0 +1,32 @@
+//Exemplo 14 - Repita Até (Do while)
+
+#include <stdio.h>
+#include <string.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main(){
+
+    double C, F;
+    char resp;
+
+    do{
+        printf("Digite a temperatura em Celsius: ");
+        scanf("%lf", &C);
+
+        F = 9.0 * C / 5.0 + 32.0;
+
+        printf("Equivalente em Fahrenheit: %.1lf\n", F);
+
+        printf("Deseja repetir (s/n)? ");
+        limpar_entrada();
+        scanf("%c", &resp);
+
+    } while(resp == 's');
+
+    return 0;
+
+}
